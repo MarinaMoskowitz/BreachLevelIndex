083c82429b12f4751e8fe720b157aace401feef7
reillyknight <reillyknight@yahoo.co.uk>
2017-11-07 20:15:46 +0000

    Update to data files

--- explore.py
+++ explore.py
@@ -31,7 +31,7 @@ def main(company,
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
-        main(sys.argv[1])
+        print(main(sys.argv[1]))
     else:
         print("Please provide victim search name")
 
